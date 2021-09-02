a6699b4815aeedb973fdb1ff8688202c8856dd8e
Pavel <pavelguest@yandex.ru>
2021-09-02 23:43:25 +0300

    feat: add func

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
+      this.center = Math.round((this.min + this.max) / 2);
 
+      return this.center;
     }
 
     lower() {
-
+      this.max = this.center;
     }
 
     greater() {
-
+      this.min = this.center;
     }
 }
 
